628e74bb7cfd8abefa1e4fe5631cc76fe6239990
Nick Finley-Day <nickfday@gmail.com>
2015-10-21 15:57:59 +0100

    Readde dprev and next

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -16,14 +16,27 @@ angular.module('portfolioAppApp')
     ];
         $scope.fromFactory = wpjsonFactory.sayHello("World");
 
-        $http.get("http://portfolio-remote.dev/wp-json/media?parent?type=project")
+        $http.get("http://portfolio-remote.dev/wp-json/media?parent?type=projecta")
+        //$http.get("http://portfolio-2015.app/wp-json/media?parent?type=project")
             .success(function(response) {$scope.projects = response;});
 
-        $scope.test = function (project) {
-            $scope.selectedTest = project;
-        };
+        //$scope.test = function (project) {
+        //    $scope.selectedTest = project;
+        //};
+
         $scope.close = function() {
             $scope.shown = false;
+        };
+        $scope.next = function() {
+          console.log('NEXT CLICK');
+          $scope.test();
+        };
+        $scope.prev = function() {
+            console.log('PREV CLICK');
+        };
+
+        $scope.select= function(index) {
+            $scope.selected = index;
         };
 
         $scope.selectedTest = null;
@@ -31,11 +44,12 @@ angular.module('portfolioAppApp')
         $scope.dataLoaded = true;
 
         $scope.test = function (project) {
+            console.log('TEST CLICK');
             $scope.selectedTest = project;
             $scope.shown = true;
+
             //window.location.href = '/#projects';
             //var currentSlide = $('.slick-slider').slick('slickCurrentSlide');
-
         };
 
   });
