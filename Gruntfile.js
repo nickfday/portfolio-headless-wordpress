9f179b32f8e386cf514061ae30418118a94cdcf9
Nick Finley-Day <nickfday@gmail.com>
2015-10-27 17:25:51 +0000

    amended gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -337,7 +337,7 @@ module.exports = function (grunt) {
         options: {
           collapseWhitespace: true,
           conservativeCollapse: true,
-          collapseBooleanAttributes: true,
+          collapseBooleanAttributes: false,
           removeCommentsFromCDATA: true
         },
         files: [{
