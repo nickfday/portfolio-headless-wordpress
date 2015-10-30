e5e870ce428007b8987d5c7365b8f989398e6e78
Nick Finley-Day <nickfday@gmail.com>
2015-10-30 01:16:11 +0000

    added style tweaks

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,13 +8,13 @@
  * Controller of the portfolioAppApp
  */
 angular.module('portfolioAppApp')
+  .value('duScrollBottomSpy', true)
   .controller('MainCtrl', function ($scope, wpjsonFactory, $http, $rootScope) {
     this.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-
         $http.get("http://finley-day.com/wp-json/media?parent?type=project")
         .success(function(response) {$scope.projects = response;});
 
